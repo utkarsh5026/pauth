0e3a46d507b7658d065cbb16edba019b4240c409
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2024-07-19 03:53:08 +0530

    Added setup for package
    Signed-off-by: Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pauth",
+    version="0.1.0",
+    author="Utkarsh Priyadarshi",
+    author_email="utkarshpriyadarshi5026@gmail.com",
+    description="A Python library for handling OAuth 2.0 authentication with multiple providers.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/utkarsh5026/pauth",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.12',
+    install_requires=[
+        "requests",
+    ],
+    extras_require={
+        "flask": ["Flask"],
+        "django": ["Django"],
+    },
+    include_package_data=True,
+)
