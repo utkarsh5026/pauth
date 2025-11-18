63ce8416d576b8f49673cc3cea4ff80523527596
Utkarsh Priyadarshi <therealutkarshpriyadarshi@gmail.com>
2025-11-19 02:04:38 +0530

    Refactor __init__.py: ensure proper export of provider classes in __all__ list

--- src/providers/__init__.py
+++ src/providers/__init__.py
@@ -2,3 +2,10 @@ from .base import BaseProvider
 from .facebook import FacebookProvider
 from .github import GithubProvider
 from .google import GoogleProvider
+
+__all__ = [
+    "BaseProvider",
+    "FacebookProvider",
+    "GithubProvider",
+    "GoogleProvider",
+]
