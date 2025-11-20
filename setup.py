ffb477f5e6506408130fbff6069ddac55e519204
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-11-20 12:49:43 +0530

    chore: Bump version to 0.2.0 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pauth",
-    version="0.1.0",
+    version="0.2.0",
     author="Utkarsh Priyadarshi",
     author_email="utkarshpriyadarshi5026@gmail.com",
     description="A Python library for handling OAuth 2.0 authentication with multiple providers.",
